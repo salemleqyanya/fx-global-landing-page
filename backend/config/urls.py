6f0dadf32f8614afbe353165d31c0ba9504e7d75
salem leqaynay <>
2025-11-05 13:08:20 +0200

    fixes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,17 +5,25 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import landing_page
-from django.urls import path, include ,re_path
-from django.conf import settings
+from django.urls import path, include, re_path
 from django.views.static import serve
+
 urlpatterns = [
     path('master-co/', admin.site.urls),
     path('api/contacts/', include('contacts.urls')),
     path('api/videos/', include('videos.urls')),
     path('', landing_page, name='landing_page'),
     re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-# Serve media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files (required even in production when using reverse proxy)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+        'show_indexes': False,
+    }),
+]
+
+# Serve static files in development (WhiteNoise handles static files automatically in production)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
